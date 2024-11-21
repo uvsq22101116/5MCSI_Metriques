84414756d9946a8e3ffbfcccf04cc3b6a27a06a2
Manel MOKRAB <99487238+uvsq22101116@users.noreply.github.com>
2024-11-21 17:05:57 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,6 +7,10 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)      
 
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
