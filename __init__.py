15f1c980d43901b68fb3b0d683f5f351e3b1234b
Manel MOKRAB <99487238+uvsq22101116@users.noreply.github.com>
2024-11-21 17:04:40 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
-Testez cette nouvelle route dans votre naviga
+  
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
